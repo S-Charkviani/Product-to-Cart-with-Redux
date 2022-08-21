17c11dfddd9c3aaac6f134c5c3fb88048a7ffc2e
Salome Charkviani <salo.charkviani@gmail.com>
2022-08-21 22:46:52 +0400

    21.08

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -3,13 +3,13 @@ import classes from "./Products.module.css";
 
 const DUMMY_PRODUCTS = [
   {
-    id: "p1",
+    id: '1',
     price: 6,
     title: "My First Book",
     description: "The first book I ever wrote",
   },
   {
-    id: "p2",
+    id: '2',
     price: 5,
     title: "My Last Book",
     description: "The last book I ever wrote",
@@ -24,6 +24,7 @@ const Products = (props) => {
         {DUMMY_PRODUCTS.map((product) => (
           <ProductItem
             key={product.id}
+            id={product.id}
             title={product.title}
             price={product.price}
             description={product.description}
